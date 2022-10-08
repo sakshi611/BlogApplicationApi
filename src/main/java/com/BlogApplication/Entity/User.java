93af0ec528a5fb3c146d8441186fbec1a4f399bd
sakshi611 <sakshi.casper@gmail.com>
2022-10-08 16:52:16 -0700

    initial commit

---
+++ src/main/java/com/BlogApplication/Entity/User.java
@@ -0,0 +1,32 @@
+package com.BlogApplication.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Table(name = "users")
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String about;
+}
---
+++ src/main/java/com/BlogApplication/Payload/UserDto.java
@@ -0,0 +1,17 @@
+package com.BlogApplication.Payload;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+
+	private String name;
+	private String email;
+	private String password;
+	private String about;
+}
