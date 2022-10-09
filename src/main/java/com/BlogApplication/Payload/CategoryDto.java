d5441527e958d41fb89ac3dadbe3e52ed5605189
sakshi611 <sakshi.casper@gmail.com>
2022-10-09 07:16:33 -0700

    validation added in category dto

--- src/main/java/com/BlogApplication/Payload/CategoryDto.java
+++ src/main/java/com/BlogApplication/Payload/CategoryDto.java
@@ -1,5 +1,8 @@
 package com.BlogApplication.Payload;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +14,10 @@ public class CategoryDto {
 
 	private Integer categoryId;
 	
+	@NotBlank
+	@Size(min = 3, message ="Min size of category title is 3 Characters.")
 	private String categoryTitle;
 	
+	@NotBlank
 	private String categoryDescription;
 }
